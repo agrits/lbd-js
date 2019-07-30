4ee3caf362591f5abbe8afef8b5dbfba1e1610fc
Artur Kasza <arturkasza98@gmail.com>
2019-07-30 13:47:04 +0200

    Calculator done. All tests fail. Dunno why (:

---
+++ calculator/calc.js
@@ -0,0 +1,63 @@
+
+function numberClicked(number){
+  if(mode === "equals"){
+    current = ""
+    mode = "number"
+    numberClicked(number)
+  }
+  else{
+    if(mode === "operation"){
+      ans = current
+      current = ""
+    }
+    mode = "number"
+    current = parseFloat(current + "" + number)
+    updateDisplay(current)
+  }
+}
+
+function operationClicked(op){
+  if(mode === "number" && !(!ans) && !(!current) ){
+    equalsClicked()
+  }
+  mode = "operation"
+  operation = op
+  ans = current
+}
+function equalsClicked(){
+  current = ""+eval(ans+operation+current)
+  ans = ""
+  mode = "equals"
+  updateDisplay(current)
+}
+function updateDisplay(string){
+  $("#display").val(string)
+}
+function clearClicked(){
+  updateDisplay("")
+  current = ""
+  ans = ""
+  operation = "+"
+  mode = "number"
+}
+function initHandlers(){
+  for(let i = 0; i<=9; i++){
+    $("#button"+i).on('click', function(){numberClicked(i)})
+  }
+
+  $("#equalsButton").on('click', equalsClicked)
+  $("#multiplyButton").on('click', function(){operationClicked("*")})
+  $("#addButton").on('click', function(){operationClicked("+")})
+  $("#subtractButton").on('click', function(){operationClicked("-")})
+  $("#divideButton").on('click', function(){operationClicked("/")})
+  $("#clearButton").on('click', clearClicked)
+}
+
+$(document).ready(function(){
+  mode = "equals"
+  ans = ""
+  operation = "+"
+  current = ""
+  initHandlers()
+  updateDisplay("")
+})
